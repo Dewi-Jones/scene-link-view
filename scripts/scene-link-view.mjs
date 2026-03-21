4008470da584cea473bf79dafb2f407bb52d73c5
Dewi-Jones <53094799+Dewi-Jones@users.noreply.github.com>
2026-03-21 16:26:25 +0100

    Add files via upload

---
+++ scripts/scene-link-view.mjs
@@ -0,0 +1,60 @@
+function newOnClickDocumentLink(event) {
+	
+	const activateScene = event.shiftKey && game.settings.get("scene-link-view", "shiftActivate") && game.user.isGM;
+	const viewJournal = event.ctrlKey && game.settings.get("scene-link-view", "ctrlJournal");
+	const viewScene = game.user.isGM || ( this.ownership["default"] == 2 && game.settings.get("scene-link-view", "playerView") );
+	
+	if ( activateScene ) {
+		game.scenes.get(this._id).activate();
+		return;
+	}
+		
+	if ( viewJournal ) {
+		if ( !this.journal ) {
+			ui.notifications.warn("SCENE_LINK_VIEW.WARNING.noJournal", {localize: true, console: false});
+			return
+		} else return this.journal._onClickDocumentLink(event)
+	}
+		
+	if ( viewScene ) {
+		game.scenes.get(this._id).view();
+		return;
+	}
+	
+	ui.notifications.warn("SCENE_LINK_VIEW.WARNING.noView", {localize: true, console: false});
+}
+
+Hooks.once("init", () => {
+	
+	game.settings.register("scene-link-view", "shiftActivate", {
+		name: "SCENE_LINK_VIEW.SETTINGS.shiftActivate.name",
+		hint: "SCENE_LINK_VIEW.SETTINGS.shiftActivate.hint",
+		scope: "world",
+		config: true,
+		requiresReload: false,
+		type: Boolean,
+		default: true
+	});
+	
+	game.settings.register("scene-link-view", "ctrlJournal", {
+		name: "SCENE_LINK_VIEW.SETTINGS.ctrlJournal.name",
+		hint: "SCENE_LINK_VIEW.SETTINGS.ctrlJournal.hint",
+		scope: "world",
+		config: true,
+		requiresReload: false,
+		type: Boolean,
+		default: true
+	});
+	
+	game.settings.register("scene-link-view", "playerView", {
+		name: "SCENE_LINK_VIEW.SETTINGS.playerView.name",
+		hint: "SCENE_LINK_VIEW.SETTINGS.playerView.hint",
+		scope: "world",
+		config: true,
+		requiresReload: false,
+		type: Boolean,
+		default: true
+	});
+	
+	Scene.prototype._onClickDocumentLink = newOnClickDocumentLink;
+});
