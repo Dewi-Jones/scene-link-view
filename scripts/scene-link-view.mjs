78ef42d9b9441601579bcb1f4208f29a73106bbf
Dewi-Jones <53094799+Dewi-Jones@users.noreply.github.com>
2026-04-02 13:12:26 +0200

    ALT modifier key to open the scene config

--- scripts/scene-link-view.mjs
+++ scripts/scene-link-view.mjs
@@ -3,28 +3,21 @@ function newOnClickDocumentLink(event) {
 	const activateScene = event.shiftKey && game.settings.get("scene-link-view", "shiftActivate") && game.user.isGM;
 	const viewJournal = event.ctrlKey && game.settings.get("scene-link-view", "ctrlJournal");
 	const viewScene = game.user.isGM || ( this.ownership["default"] == 2 && game.settings.get("scene-link-view", "playerView") );
+	const viewConfig = event.altKey && game.settings.get("scene-link-view", "altConfig");
 	
-	if ( this.uuid.startsWith("Compendium") ) {
-		ui.notifications.warn("SCENE_LINK_VIEW.WARNING.compendium", {localize: true});
-		return;
-	}
+	if ( viewConfig ) return this.sheet.render(true)
+	
+	if ( this.uuid.startsWith("Compendium") ) return ui.notifications.warn("SCENE_LINK_VIEW.WARNING.compendium", {localize: true})
 	
 	if ( viewJournal ) {
 		if ( !this.journal ) {
-			ui.notifications.warn("SCENE_LINK_VIEW.WARNING.noJournal", {localize: true, console: false});
-			return;
+			return ui.notifications.warn("SCENE_LINK_VIEW.WARNING.noJournal", {localize: true, console: false});
 		} else return this.journal._onClickDocumentLink(event)
 	}
 	
-	if ( activateScene ) {
-		game.scenes.get(this.id).activate();
-		return;
-	}
+	if ( activateScene ) return game.scenes.get(this.id).activate()
 
-	if ( viewScene ) {
-		game.scenes.get(this.id).view();
-		return;
-	}
+	if ( viewScene ) return game.scenes.get(this.id).view()
 	
 	ui.notifications.warn("SCENE_LINK_VIEW.WARNING.noView", {localize: true, console: false});
 }
@@ -54,6 +47,16 @@ Hooks.once("init", () => {
 	game.settings.register("scene-link-view", "playerView", {
 		name: "SCENE_LINK_VIEW.SETTINGS.playerView.name",
 		hint: "SCENE_LINK_VIEW.SETTINGS.playerView.hint",
+		scope: "world",
+		config: true,
+		requiresReload: false,
+		type: Boolean,
+		default: true
+	});
+	
+	game.settings.register("scene-link-view", "altConfig", {
+		name: "SCENE_LINK_VIEW.SETTINGS.altConfig.name",
+		hint: "SCENE_LINK_VIEW.SETTINGS.altConfig.hint",
 		scope: "world",
 		config: true,
 		requiresReload: false,
